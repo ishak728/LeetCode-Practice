5b44c0a06a6941debdf5db4e2298430af6321fda
Ishak <erdoganishak333@gmail.com>
2024-10-01 22:40:51 -0400

    day 7

--- Remove Element.js
+++ Remove Element.js
@@ -6,23 +6,38 @@
 //Input: nums = [0,1,2,2,3,0,4,2], val = 2
 var removeElement = function(nums, val) {
 
-    let k=-1
-    for(let i=0;i<nums.length;i++){
-       
-        if(nums[i]===2 && k===-1){
-            k=i
-        }
-        if(nums[i]!==2){
-            nums[k]=nums[i]
-            k=-1
-        }
-    }
+    let p=-1
+    let count=0
 
-    return k
+    for(let i=0;i<nums.length;i++){
+
+       
+
+        if(p===-1 && nums[i]===val){
+            p=i
+            continue
+        }
+
+        if(p!==-1 && nums[i]!==val){
+            nums[p]=nums[i]
+            nums[i]=val
+            i=p
+            p=-1
+            
+            
+        }
+
+        if(nums[i]!==val){
+            count++
+        }
+        
+    }
+  
+ return count
     
 };
 
 
-console.log(removeElement(nums = [0,1,2,2,3,0,4,2]))
+console.log(removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
 
 
